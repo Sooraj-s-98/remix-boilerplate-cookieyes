65a340e453b76a4931c55fb873911b8e301d5282
Sooraj S <52352285+Sooraj-s-98@users.noreply.github.com>
2023-11-02 09:17:58 +0530

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -22,6 +22,7 @@ export default function App() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/536f1cb9dc5c1e5d4b9b45dc/script.js"></script>
       </head>
       <body>
         <Outlet />
